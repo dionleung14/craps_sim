48390adffdd77c722dd4e1dd5722bf63e0289c8b
Dion L <dioncleung@gmail.com>
2022-03-30 23:33:49 -0700

    first commit from command line

---
+++ die.py
@@ -0,0 +1,5 @@
+import random
+
+def roll_die():
+    die_num = random.randint(1,6)
+    return die_num
---
+++ game.py
@@ -0,0 +1,6 @@
+from die import roll_die
+
+dice1 = roll_die()
+dice2 = roll_die()
+roll = dice1 + dice2
+print(roll)
